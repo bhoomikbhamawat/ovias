02e51dde00f4e652cd2038ef5d6fe226dc27c0b0
Akash Verma <av9541742@gmail.com>
2019-01-09 23:18:00 +0530

    menu.xml created and logout activity sorted

--- app/src/main/java/com/example/akash/login/Calender.java
+++ app/src/main/java/com/example/akash/login/Calender.java
@@ -1,16 +1,28 @@
 package com.example.akash.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Calender extends AppCompatActivity {
     private TextView d1;
     private CalendarView mCalendar;
+    private Button lout;
     String date;
+    private FirebaseAuth fbase;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +40,54 @@ public class Calender extends AppCompatActivity {
                 d1.setText(date);
 
 
+
+
             }
 
         });
+        fbase =FirebaseAuth.getInstance();
+        lout=(Button)findViewById(R.id.out);
+
+        lout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbase.signOut();
+                finish();
+                Intent intent =new Intent(Calender.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
+    private void logout(){
+        fbase.signOut();
+        finish();
+        Intent intent =new Intent(Calender.this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logoutmenu:{
+                logout();
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+
+
+    }
+
+
 
 }
--- app/src/main/java/com/example/akash/login/MainActivity.java
+++ app/src/main/java/com/example/akash/login/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.akash.login;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +10,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private EditText Name;
     private EditText Pass;
     private Button Login;
     private TextView ureg;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog pdilog;
 
 
 
@@ -27,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
         Pass =(EditText)findViewById(R.id.p1);
         Login =(Button) findViewById(R.id.b1);
         ureg=(TextView) findViewById(R.id.t1);
+        firebaseAuth=FirebaseAuth.getInstance();
+        pdilog=new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this,Calender.class));
+        }
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 valid(Name.getText().toString(), Pass.getText().toString());
             }
         });
+
         ureg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,11 +67,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void  valid(String userName, String pswd){
-        if((userName.equals ("akash")) && (pswd.equals ("123"))){
-            Intent intent = new Intent(MainActivity.this,Calender.class);
-            Log.e("intent" ,"passed");
-            startActivity(intent);
-        }
+        pdilog.setMessage("Welcome  Validating...");
+        pdilog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName,pswd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    pdilog.dismiss();
+                    Toast.makeText(MainActivity.this,"Login successfull",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,Calender.class));
+                }else {
+                    pdilog.dismiss();
+                    Toast.makeText(MainActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         }
     }
